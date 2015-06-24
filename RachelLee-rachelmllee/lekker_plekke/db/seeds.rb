c0bcd073956e31c7d0fc85cecce2c128e30b4902
rachelmllee <rachel.ml.lee@gmail.com>
2015-06-24 06:43:13 -0700

    places index works

--- RachelLee-rachelmllee/lekker_plekke/db/seeds.rb
+++ RachelLee-rachelmllee/lekker_plekke/db/seeds.rb
@@ -1,7 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Place.delete_all
+
+Place.create! name: 'Rhodes Memorial', activity: 'beautiful views', visited: true
+Place.create! name: 'Old Biscuit Mill', activity: 'day and night markets', district: 'Woodstock', visited: true
+Place.create! name: 'Table Mountain', activity: 'hiking', visited: false
+Place.create! name: 'Sababa', activity: 'restaurant', visited: true
+Place.create! name: 'Lions Head', activity: 'hiking', visited: true
+Place.create! name: 'Waterfront', activity: 'shopping, dining', visited: true
